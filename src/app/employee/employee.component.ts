991961246f7e4ee391d344e6e1908744542793b5
syedareehaquasar <reeha.saba@gmail.com>
2021-10-20 02:52:14 +0530

    employee assignment

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -3,25 +3,60 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent implements OnInit {
-
-  employee1: any;
-  employee2: any;
-  employee3: any;
-  employee4: any;
-  employee5: any;
+  employees: any;
 
   constructor() {
-    this.employee1 = {id: 1, name: "reeha", gender: 'F', salary: 800000000, DOJ: "12-10-2021", loginId: "reeha", password: "reeha"};
-    this.employee2 = {id: 2, name: "reeha1", gender: 'M', salary: 8000000, DOJ: "10-10-2021", loginId: "reeha1", password: "reeha1"};
-    this.employee3 = {id: 3, name: "reeha2", gender: 'F', salary: 8000, DOJ: "01-10-2021", loginId: "reeha2", password: "reeha2"};
-    this.employee4 = {id: 4, name: "reeha3", gender: 'M', salary: 80000000000, DOJ: "09-10-2021", loginId: "reeha3", password: "reeha3"};
-    this.employee5 = {id: 5, name: "reeha4", gender: 'F', salary: 80, DOJ: "21-10-2021", loginId: "reeha4", password: "reeha4"};
+    this.employees = [
+      {
+        id: 1,
+        name: 'reeha',
+        gender: 'F',
+        salary: 800000000,
+        DOJ: '12-10-2021',
+        loginId: 'reeha',
+        password: 'reeha',
+      },
+      {
+        id: 2,
+        name: 'reeha1',
+        gender: 'M',
+        salary: 8000000,
+        DOJ: '10-10-2021',
+        loginId: 'reeha1',
+        password: 'reeha1',
+      },
+      {
+        id: 3,
+        name: 'reeha2',
+        gender: 'F',
+        salary: 8000,
+        DOJ: '01-10-2021',
+        loginId: 'reeha2',
+        password: 'reeha2',
+      },
+      {
+        id: 4,
+        name: 'reeha3',
+        gender: 'M',
+        salary: 80000000000,
+        DOJ: '09-10-2021',
+        loginId: 'reeha3',
+        password: 'reeha3',
+      },
+      {
+        id: 5,
+        name: 'reeha4',
+        gender: 'F',
+        salary: 80,
+        DOJ: '21-10-2021',
+        loginId: 'reeha4',
+        password: 'reeha4',
+      },
+    ];
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/resgister/resgister.component.ts
+++ src/app/resgister/resgister.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResgisterComponent implements OnInit {
 
-  constructor() { }
+  employees: any;
+  loginId: String;
+  password: String;
+
+  constructor() {
+    this.loginId = "";
+    this.password = "";
+    this.employees = [
+
+    ]
+  }
 
   ngOnInit(): void {
   }
 
+  loginSubmit(): void {
+    // alert("Login credentials are submitted");
+    if (this.loginId === 'HR' && this.password === 'HR') {
+      alert(' Welcome to HR Home Page... ');
+    } else {
+      this.employees.array.forEach((employee: any) => {
+        if ((this.loginId === employee.email) && (this.password === employee.password)) {
+          alert(' Welcome to Employee Home Page... ');
+        } else {
+          alert(' Login Failure');
+        }
+      });
+    }
+  }
 }
